63662ab6cfbb77019f0b8f82fb215a97a4192be2
TRP <t@trp.dev>
2026-05-20 20:46:24 -0600

    get routes

--- src/controllers/bootcamps.ts
+++ src/controllers/bootcamps.ts
@@ -1,19 +1,47 @@
 import type { Request, Response, NextFunction } from "express";
 import { Bootcamp } from "../models/Bootcamp.js";
 
-export const getBootcamps = (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({
-    success: true,
-    message: "Show bootcamps",
-    signature: req.signature, // Access the custom property added by the logger middleware
-  });
+export const getBootcamps = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({
+            success: true,
+            data: bootcamps
+        });
+    } catch (err: unknown) {
+        const errorMessage = err instanceof Error ? err.message : String(err);
+
+        res.status(400).json({
+            success: false,
+            error: errorMessage
+        });
+    }   
 };
 
-export const getBootcamp = (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({
-    success: true,
-    message: "Show bootcamp with id: " + req.params.id,
-  });
+export const getBootcamp = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            return res.status(404).json({
+                success: false,
+                error: "Bootcamp not found with id of " + req.params.id
+            });
+        }
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        });
+    } catch (err: unknown) {
+        const errorMessage = err instanceof Error ? err.message : String(err);
+
+        res.status(400).json({
+            success: false,
+            error: errorMessage
+        });
+    }   
+
 };
 
 export const createBootcamp = async (req: Request, res: Response, next: NextFunction) => {
