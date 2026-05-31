28afa5de9c93d187ae101bc94f8acb8e2fd39dcc
TRP <t@trp.dev>
2026-05-30 21:52:35 -0600

    Additional error handling

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import { Error as MongooseError } from "mongoose";
+import { MongoServerError } from "mongodb";
 import { ErrorResponse } from "../utils/errorResponse.js";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -11,6 +12,16 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
     if(err instanceof MongooseError.CastError) {
         const message = `Resource not found with id of ${err.value}`;
         error = new ErrorResponse(message, 404);
+    }
+
+    if (err instanceof MongoServerError && err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    if (err instanceof MongooseError.ValidationError) {
+        const message = Object.values(err.errors).map(val => val.message).join(", ");
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
